618c053ebe3e6ba58c23afbd52d32780889a685e
MartinAllein <martin.github@alleingespielt.de>
2023-07-28 20:12:44 +0200

    ItemBoxPartition.py work in Progress

---
+++ classes/ConfigConstants.py
@@ -0,0 +1,14 @@
+
+class ConfigConstants:
+
+    # ConfigConstants for Project.py
+    config_file = "config file"
+    config_section = "config section"
+    config_file_extension = "config"
+
+    # general ConfigConstants
+    x_offset = "x offset"
+    y_offset = "y offset"
+    width = "width"
+    length = "length"
+    height = "height"
