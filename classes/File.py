a2ed1e370fd1f932256b7d6daa88fdb1a0e33a69
MartinAllein <martin.github@alleingespielt.de>
2023-08-09 16:43:17 +0200

    File.py single quotes as standard.

--- classes/File.py
+++ classes/File.py
@@ -12,16 +12,16 @@ class File:
 
     @staticmethod
     def set_svg_extension(filename: str) -> str:
-        return File.set_file_extension(filename, "svg")
+        return File.set_file_extension(filename, 'svg')
 
     @staticmethod
     def set_config_extension(filename: str) -> str:
-        return File.set_file_extension(filename, "config")
+        return File.set_file_extension(filename, 'config')
 
     @staticmethod
     def set_file_extension(filename: str, extension: str) -> str:
         if not extension[0] == '.':
-            extension = "." + extension
+            extension = '.' + extension
 
         # Set extension of filename to requested extension
         if filename[-(len(extension)):] != extension:
